ab0050529cac2305e1158248fe30d94437bff858
Matheus Grandinetti <matheusgrandinetti@gmail.com>
2025-06-16 23:37:37 -0300

    added example generator and input read

--- models/gpt_client.py
+++ models/gpt_client.py
@@ -8,47 +8,45 @@ from models.base_client import BaseClient
 
 load_dotenv()
 
+
 class GPTClient(BaseClient):
     def __init__(self):
-        self.client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-    
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
     def generate_prompt(self, template, **kwargs):
         formatted = template.format(**kwargs)
         system_match = self.find_pattern(formatted, "SYSTEM_CONFIG")
         user_match = self.find_pattern(formatted, "USER_CONFIG")
-        
+
         system_config = self.generate_request_dict("system", system_match[0])
         user_config_1 = self.generate_request_dict("user", user_match[0])
-        
+
         if kwargs["PROMPT"] != "one_shot":
             return [system_config, user_config_1]
-        
+
         assistant_match = self.find_pattern(formatted, "ASSISTANT_CONFIG")
         assistant_config = self.generate_request_dict("assistant", assistant_match[0])
-        
+
         user_config_2 = self.generate_request_dict("user", user_match[1])
-        
+
         return [system_config, user_config_1, assistant_config, user_config_2]
-    
+
     def process(self, args):
         try:
+            input_code = self.get_input_code(file_path=args.INPUT_PATH)
             template = self.load_template(file_name=args.PROMPT)
             prompt = self.generate_prompt(
                 template=template,
                 PROMPT=args.PROMPT,
                 LANGUAGE_NAME=args.LANGUAGE_NAME,
                 OLD_LIB_NAME=args.OLD_LIB_NAME,
-                NEW_LIB_NAME=args.NEW_LIB_NAME
+                NEW_LIB_NAME=args.NEW_LIB_NAME,
+                CODE_BEFORE_MIGRATION=input_code,
             )
-            if not prompt:
-                response = self.client.chat.completions.create(
-                    model=args.VERSION,
-                    messages=prompt
-                )
-                return response.choices[0].message.content
-            return prompt
+
+            response = self.client.chat.completions.create(
+                model=args.VERSION, messages=prompt
+            )
+            return response.choices[0].message.content
         except ValueError as e:
             return f"{self.__class__.__name__} could not generate a response: {e}"
-        
-        
-        
--- models/ollama_client.py
+++ models/ollama_client.py
@@ -2,10 +2,11 @@ import ollama
 
 from models.base_client import BaseClient
 
+
 class OllamaClient(BaseClient):
     def __init__(self):
         self.client = ollama
-    
+
     def generate_prompt(self, template, **kwargs):
         formatted = template.format(**kwargs)
         system_match = self.find_pattern(formatted, "SYSTEM_CONFIG")
@@ -16,28 +17,27 @@ class OllamaClient(BaseClient):
 
         if kwargs["PROMPT"] != "one_shot":
             return [system_config, user_config_1]
-        
+
         assistant_match = self.find_pattern(formatted, "ASSISTANT_CONFIG")
         assistant_config = self.generate_request_dict("assistant", assistant_match[0])
 
         user_config_2 = self.generate_request_dict("user", user_match[1])
 
         return [system_config, user_config_1, assistant_config, user_config_2]
-    
+
     def process(self, args):
         try:
+            input_code = self.get_input_code(file_path=args.INPUT_PATH)
             template = self.load_template(file_name=args.PROMPT)
             prompt = self.generate_prompt(
                 template=template,
                 PROMPT=args.PROMPT,
                 LANGUAGE_NAME=args.LANGUAGE_NAME,
                 OLD_LIB_NAME=args.OLD_LIB_NAME,
-                NEW_LIB_NAME=args.NEW_LIB_NAME
+                NEW_LIB_NAME=args.NEW_LIB_NAME,
+                CODE_BEFORE_MIGRATION=input_code,
             )
-            response = self.client.chat(
-                model=args.VERSION,
-                messages=prompt
-            )
-            return response['message']['content']
+            response = self.client.chat(model=args.VERSION, messages=prompt)
+            return response["message"]["content"]
         except ValueError as e:
             return f"{self.__class__.__name__} could not generate a response: {e}"
